f002f30e54a9aa7d82a05b9ed6189252cfc02ff5
Eric di Domenico <eric@didomenico.com>
2017-08-08 15:38:13 -0400

    Update brew-go-jira.rb

--- brew-go-jira.rb
+++ brew-go-jira.rb
@@ -3,16 +3,15 @@ class GoJira < Formula
   homepage "https://github.com/Netflix-Skunkworks/go-jira"
 
   # curl -L -o hoge https://github.com/Netflix-Skunkworks/go-jira/releases/download/0.0.19/jira-darwin-38 && chmod +x hoge && ./hoge -h
-  url "https://github.com/Netflix-Skunkworks/go-jira/releases/download/0.0.19/jira-darwin-386"
-
+  url "https://github.com/Netflix-Skunkworks/go-jira/releases/download/v0.1.14/jira-darwin-10.6-amd64"
   # shasum -a 256 thefile
-  sha256 "c7b6c9ff974c7120bb721579ca53fc6d9c7e8537f50f2d8c18b7c74deec58896"
+  #sha256 "c7b6c9ff974c7120bb721579ca53fc6d9c7e8537f50f2d8c18b7c74deec58896"
 
   # See https://github.com/Netflix-Skunkworks/go-jira/releases
-  version "0.0.19"
+  version "0.1.14"
 
   def install
-    bin.install "jira-darwin-386" => "jira"
+    bin.install "jira-darwin-10.6-amd64" => "jira"
   end
 
   def caveats; <<-EOS.undent
@@ -21,8 +20,7 @@ class GoJira < Formula
       mkdir ~/.jira.d
 
       cat <<EOM >~/.jira.d/config.yml
-      endpoint: https://jira.mycompany.com or https://mycompany.atlassian.net
-      user: login user
+      endpoint: https://mycompany.atlassian.net
       EOM
 
     If you are working on a "foo" project, create a configuration under your project workspace at ./.jira.d/config.yml that looks like:
